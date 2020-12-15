3eda1a165d0c689ca37dc007b3a56a2ae3582b11
Tom Chestnut <tchestnut85@gmail.com>
2020-12-15 18:11:18 -0500

    added module code files due to errors.

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
-const path = require('path');
-const webpack = require('webpack');
-require('bootstrap');
-require('popper.js');
+const webpack = require("webpack")
+const path = require("path");
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    },
-    mode: 'development',
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-    ]
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
 };
